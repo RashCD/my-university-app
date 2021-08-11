9e4af85d82750a474c1217d7ab5f17d692314c32
CD <mrrash91@gmail.com>
2021-08-12 03:11:08 +0800

    setup static file for client build

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,25 @@
 import express, { Express, Request, Response } from 'express';
-import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app: Express = express();
 
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/api', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
