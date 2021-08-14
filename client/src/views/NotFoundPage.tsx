2a11d3789de947f4a1b291bb899aa4c64fee9502
CD <mrrash91@gmail.com>
2021-08-15 02:08:21 +0800

    edit not found pages

--- client/src/views/NotFoundPage.tsx
+++ client/src/views/NotFoundPage.tsx
@@ -1,8 +1,14 @@
 import { RouteComponentProps } from '@reach/router';
-import React from 'react';
+import Styles from '../assets/styles/views/NotFoundPage.module.scss';
+import AppHeader from '../components/AppHeader';
 
 const NotFoundPage = (props: RouteComponentProps) => {
-  return <div>404 Not found</div>;
+  return (
+    <div className="app">
+      <AppHeader />
+      <div className={Styles.notFound}>404 | Not found</div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
