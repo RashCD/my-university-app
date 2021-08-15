533ca703cf534376a6e421d8a2e21bb99b5ad062
CD <mrrash91@gmail.com>
2021-08-16 02:14:06 +0800

    able to save info to a file using fs server

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import fs from 'fs';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -16,6 +17,35 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/api', (req: Request, res: Response) => {
   res.json({ message: 'Hello from server!' });
+});
+
+app.post('/api/login', (req: Request, res: Response) => {
+  res.json({ message: req.body });
+});
+
+app.post('/api/signup', (request: Request, respond: Response) => {
+  const filePath = './user.json';
+
+  fs.readFile(filePath, { encoding: 'utf8', flag: 'a+' }, (error, data) => {
+    if (error) {
+      throw error;
+    }
+
+    const oldContent = data && JSON.parse(data);
+    const newContent = request.body;
+    const mergeContent = data
+      ? JSON.stringify([...oldContent, newContent], null, 4)
+      : JSON.stringify([newContent], null, 4);
+
+    return fs.writeFile(filePath, mergeContent, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('The file was saved!');
+    });
+  });
+
+  respond.json({ message: 'User registered' });
 });
 
 app.get('*', (req, res) => {
